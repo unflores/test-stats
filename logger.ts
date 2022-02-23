b4c1aa4669e40f85f2f2e655d1f571563953057d
Austin Flores <ctrl4ltdeleteme@gmail.com>
2022-02-23 15:16:14 +0100

    Support debugging

--- logger.ts
+++ logger.ts
@@ -6,6 +6,9 @@ export function error(message?: any, ...optionalParams: any[]) {
   console.error(message, optionalParams)
 }
 
+export function debug(message?: any, ...optionalParams: any[]) {
+  console.debug(message, optionalParams)
+}
 
 export function log(message?: any, ...optionalParams: any[]) {
   console.log(message, optionalParams)
